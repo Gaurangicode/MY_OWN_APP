c2b197c7fb36f85464d72bdcb8d462e50d867e5c
Gaurangicode <78476056+Gaurangicode@users.noreply.github.com>
2021-07-24 11:13:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var stBg,rhymesImg,learningImg,swarImg,vyanjanImg,countingImg,alphabetsImg;
+var gameState=0;
+
+function preload(){
+    stBg=loadImage("IMAGES/bg1.jpg")
+    rhymesImg=loadImage("IMAGES/rhymes.png")
+    learningImg=loadImage("IMAGES/LEARNING.PNG")
+    swarImg=loadImage("IMAGES/swar.png")
+    vyanjanImg=loadImage("IMAGES/vyanjan.png")
+    countingImg=loadImage("IMAGES/counting.jpg")
+    alphabetsImg=loadImage("IMAGES/alphabets.jpg")
+}
+function setup(){}
+function draw(){}
